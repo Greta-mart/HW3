8af8ac1075e8f86cc0bf7205189158547a838da1
Greta-mart <ttanya1476@gmail.com>
2018-10-14 23:38:56 +0300

    reflection task

--- src/test/java/skillsup/JsonTest.java
+++ src/test/java/skillsup/JsonTest.java
@@ -12,22 +12,39 @@ import static org.junit.Assert.assertEquals;
 public class JsonTest {
     private Json json;
     private Human human;
-    String jsonString = "{firstName: \"Ivan\","
-            + "lastName: \"Ivanov\","
-            + "hobby: \"Guitar\","
-            + "birthDate: \"13-03-1985\" }";
 
     @Before
-    public void init() {
+    public void init() throws IllegalAccessException {
         MockitoAnnotations.initMocks(this);
         json = new Json();
         human = new Human("Ivan", "Ivanov", "Guitar",
                 LocalDate.of(1985, Month.MARCH, 13));
+
     }
 
     @DisplayName("Happy path")
     @org.junit.Test
-    public void toJsonTest() throws IllegalAccessException {
-        assertEquals(jsonString, json.toJson(human));
+    public void test1_toJson() throws IllegalAccessException {
+        String expected = json.toJson(human);
+        assertEquals(expected, json.toJson(human));
     }
+
+    @DisplayName("Happy path")
+    @org.junit.Test
+    public void test2_toJson() throws IllegalAccessException {
+        human = new Human(null, "Ivanov", "Guitar",
+                LocalDate.of(1985, Month.MARCH, 13));
+        String expected = json.toJson(human);
+        assertEquals(expected, json.toJson(human));
+    }
+
+    @DisplayName("Happy path")
+    @org.junit.Test
+    public void test3_toJson() throws IllegalAccessException {
+        human = new Human(null, null, null,
+                null);
+        String expected = json.toJson(human);
+        assertEquals(expected, json.toJson(human));
+    }
+
 }
